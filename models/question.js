c259ad4c088d08a22758e9f3c9b40105c74933de
palman2003 <palayilmanav@gmail.com>
2023-09-09 20:43:43 +0530

    change to the deprecated modules

--- models/question.js
+++ models/question.js
@@ -3,13 +3,16 @@ const mongoose = require("mongoose");
 // Use the existing collection name "questions"
 const Question = mongoose.model("question", {}, "questions");
 
-// Example query to fetch all documents from the "questions" collection
-Question.find({}, (err, questions) => {
-  if (err) {
-    console.error("Error fetching questions:", err);
-  } else {
-    let question=[questions]
-  }
-});
+const fetchQuestions = (callback) => {
+  Question.find({}, (err, questions) => {
+    if (err) {
+      console.error("Error fetching questions:", err);
+      callback(err, null);
+    } else {
+      let question = [questions];
+      callback(null, question);
+    }
+  });
+};
 
-module.exports=Question
+module.exports = fetchQuestions;
