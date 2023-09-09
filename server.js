caae1e543dfce5eb943d614060f2cb3077fd651d
palman2003 <palayilmanav@gmail.com>
2023-09-09 10:20:24 +0530

    cors changes

--- server.js
+++ server.js
@@ -9,9 +9,15 @@ require('dotenv').config()
 
 const app= express()
 
+const corsOptions = {
+    origin: 'https://fodse-backend.onrender.com/', // Replace with your Render app URL
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+  
+app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use(cors()); 
+
 
 const port=process.env.PORT || 3000
 
